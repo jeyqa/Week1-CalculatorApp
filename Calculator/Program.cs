3e4a1b61a472d8fb7a6e19800def1e8a78f9e668
N A <jeyqa9999@gmail.com>
2025-09-29 03:52:00 +0300

    Implemented program code.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Ask the user to type the first number.
+Console.WriteLine("Type in the first number, and then press Enter");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Ask the user to type the second number.
+Console.WriteLine("Type the second number, and then press Enter");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// Perform the calculation
+int result = firstNumber + secondNumber;
+
+// Output the answer to the console
+Console.WriteLine("Adding {0} and {1} gives the answer {2}", firstNumber, secondNumber, result);
+Console.ReadKey();
